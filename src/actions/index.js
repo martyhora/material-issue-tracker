81e5aebdcf58a610cb4a684cd47faa90e76fd2d1
Martin Hora <m.hora33@gmail.com>
2017-10-06 21:38:46 +0200

    Simplify action functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,84 +13,60 @@ import {
   CANCEL_PROJECT_EDIT,
 } from './actionTypes';
 
-export const addIssue = issue => {
-  return {
-    type: ADD_ISSUE,
-    issue,
-  };
-};
+export const addIssue = issue => ({
+  type: ADD_ISSUE,
+  issue,
+});
 
-export const completeIssue = issueId => {
-  return {
-    type: COMPLETE_ISSUE,
-    issueId,
-  };
-};
+export const completeIssue = issueId => ({
+  type: COMPLETE_ISSUE,
+  issueId,
+});
 
-export const toggleFilter = () => {
-  return {
-    type: TOGGLE_FILTER,
-  };
-};
+export const toggleFilter = () => ({
+  type: TOGGLE_FILTER,
+});
 
-export const toggleEdit = issueId => {
-  return {
-    type: TOGGLE_EDIT,
-    issueId,
-  };
-};
+export const toggleEdit = issueId => ({
+  type: TOGGLE_EDIT,
+  issueId,
+});
 
-export const removeIssue = issueId => {
-  return {
-    type: REMOVE_ISSUE,
-    issueId,
-  };
-};
+export const removeIssue = issueId => ({
+  type: REMOVE_ISSUE,
+  issueId,
+});
 
-export const toggleIssueStar = issueId => {
-  return {
-    type: TOGGLE_ISSUE_STAR,
-    issueId,
-  };
-};
+export const toggleIssueStar = issueId => ({
+  type: TOGGLE_ISSUE_STAR,
+  issueId,
+});
 
-export const updateFulltextFilter = text => {
-  return {
-    type: UPDATE_FULLTEXT_FILTER,
-    text,
-  };
-};
+export const updateFulltextFilter = text => ({
+  type: UPDATE_FULLTEXT_FILTER,
+  text,
+});
 
-export const addProject = project => {
-  return {
-    type: ADD_PROJECT,
-    project,
-  };
-};
+export const addProject = project => ({
+  type: ADD_PROJECT,
+  project,
+});
 
-export const removeProject = projectId => {
-  return {
-    type: REMOVE_PROJECT,
-    projectId,
-  };
-};
+export const removeProject = projectId => ({
+  type: REMOVE_PROJECT,
+  projectId,
+});
 
-export const onProjectEditSelect = projectId => {
-  return {
-    type: TOGGLE_PROJECT_EDIT,
-    projectId,
-  };
-};
+export const onProjectEditSelect = projectId => ({
+  type: TOGGLE_PROJECT_EDIT,
+  projectId,
+});
 
-export const selectProject = projectId => {
-  return {
-    type: SELECT_PROJECT,
-    projectId,
-  };
-};
+export const selectProject = projectId => ({
+  type: SELECT_PROJECT,
+  projectId,
+});
 
-export const onProjectEditCancel = () => {
-  return {
-    type: CANCEL_PROJECT_EDIT,
-  };
-};
+export const onProjectEditCancel = () => ({
+  type: CANCEL_PROJECT_EDIT,
+});
